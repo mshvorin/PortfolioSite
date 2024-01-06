56c0d7319e8afb9e131d3730cffebce30d47a0d5
mshvorin <matthew.shvorin21@sitechhs.com>
2024-01-06 02:55:39 -0500

    Phone fixes

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
 };
+
+$(document).ready(function() {
+    if (window.matchMedia("(max-width: 950px) or (max-device-width: 480px)").matches) {
+        var lastScrollTop = 0;
+        var header = $('header');
+
+        // Initially hide the header
+        header.addClass('hidden');
+
+        $(window).scroll(function() {
+            var st = $(this).scrollTop();
+            if (st > lastScrollTop) {
+                // Scrolling down, show the header
+                header.removeClass('hidden');
+            } else {
+                // Scrolling up, hide the header
+                header.addClass('hidden');
+            }
+            lastScrollTop = st;
+        });
+    }
+});
